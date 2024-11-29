3d5d2a819c491b63bd3e000901b8849ac1529448
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-29 09:20:06 +0530

    qpo

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { sequelize } = require('./config/db');
+
 require('dotenv').config();
+const { Client } = require('pg'); // Import pg Client for PostgreSQL
 
 const authRoutes = require('./routes/authRoutes');
 const tripRoutes = require('./routes/tripRoutes');
@@ -11,6 +14,20 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
+
+// Database connection setup for PostgreSQL
+const client = new Client({
+  user: process.env.DB_USER, 
+  host: process.env.DB_HOST, 
+  database: process.env.DB_NAME, 
+  password: process.env.DB_PASSWORD, 
+  port: process.env.DB_PORT 
+});
+
+
+sequelize.authenticate()
+  .then(() => console.log('Sequelize connected to PostgreSQL successfully'))
+  .catch(err => console.error('Sequelize connection error:', err));
 
 // Routes
 app.use('/api/auth', authRoutes);
